dae0599248ce09e010aaa68fd4cc1e25f6ea4b78
Peter Persson <peter.persson@infomaker.se>
2012-11-14 09:08:22 +0100

    Refactored example code in index.php

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
 <?php
-
+//Require travlr.php
 require_once('travlr.php');
-$station = 'Kalmar C';
-$station_prefix = trim(substr(strtolower($station), 0, 4));
-apc_delete(Travlr::COMES_AROUND . $station_prefix);
 
+//Instantiate
 $my_travlr = new Travlr();
+
+//Make a call
 //$departures = $my_travlr->what_goes_around('Kalmar c');
 $arrivals = $my_travlr->what_comes_around('Kalmar c');
 
+//Work with the result
 //var_dump($departures);
-//var_dump($arrivals);
-$cache_result = apc_fetch(Travlr::COMES_AROUND . $station_prefix);
-$cache_result = json_decode($cache_result);
-foreach ( $cache_result->station->transfers->transfer as $incoming ) {
-	$cache_array[$incoming->arrival] = $incoming->origin;
-}
-var_dump($cache_array);
+var_dump($arrivals);
